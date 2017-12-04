458421406e5395b0d445c286ef878f4572a6edf9
vuducduy <vuducduy07071998@gmail.com>
2017-12-04 19:37:05 +0700

    update_Gem_Css_Js
    Add layout
    
    show_product_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/category", to: "category#index"
+  get "/product", to: "product#index"
+  get "/about", to: "static_pages#about"
+
+  root to: "static_pages#home"
 end
