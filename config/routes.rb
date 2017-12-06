98cea700a52e79406915c9493f661f604e84f904
vuducduy <vuducduy07071998@gmail.com>
2017-12-06 19:55:43 +0700

    Add_layout_home

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   get "/signup" => "users#new"
   post "/signup", to: "users#create"
   resources :users
+
+  get "static_pages/about"
+  get    "/login",   to: "sessions#new"
+  post    "/login",   to: "sessions#create"
+  delete  "/logout",  to: "sessions#destroy"
+
+  root to: "static_pages#home"
 end
